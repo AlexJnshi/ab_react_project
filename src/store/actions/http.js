0cfcf1bdfceafe10617560cb010eb9b863a604c3
Alex Shi <junnan.shi295@gmail.com>
2020-12-07 16:41:54 +1100

    remove unused plugins

--- src/store/actions/http.js
+++ src/store/actions/http.js
@@ -1,9 +1,6 @@
 import axios from "axios";
-import qs from "qs"
 const http = axios.create({
     baseURL: "https://www.adorebeauty.com.au/api/ecommerce/catalog",
-    paramsSerializer:params=>{
-        return qs.stringify(params,{arrayFormat: 'indices'})
-    }
+
 });
 export default http;
